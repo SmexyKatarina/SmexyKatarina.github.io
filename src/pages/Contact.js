b24de49a4964b525dc6301e840a4981ef7f79fd6
Kat <mercier200012@hotmail.com>
2024-07-25 22:10:52 -0400

    Remove env variables for the meantime
    - Grab env variables and store them in the page instead.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,44 +15,44 @@ const Contact = () => {
                         <h2 className="contact-type">Phone Number</h2>
                         <h3 className="contact-info" id="info-1" onClick={() => {
                             document.getElementById("info-1").style.backgroundColor = "transparent";
-                            document.getElementById("info-1").textContent = process.env.REACT_APP_PHONE_REAL;
-                        }}>{process.env.REACT_APP_PHONE_NUMBER}</h3>
+                            document.getElementById("info-1").textContent = "438-501-3972";
+                        }}>*************</h3>
                         <p className="contact-inspect" onClick={() => {
                             document.getElementById("info-1").style.backgroundColor = "transparent";
-                            document.getElementById("info-1").textContent = process.env.REACT_APP_PHONE_REAL;
+                            document.getElementById("info-1").textContent = "438-501-3972";
                         }}>Click to view phone number</p>
                     </div>
                     <div className="contact-item border-bottom">
                         <h2 className="contact-type">Hotmail (Professional)</h2>
                         <h3 className="contact-info" id="info-2" onClick={() => {
                             document.getElementById("info-2").style.backgroundColor = "transparent";
-                            document.getElementById("info-2").textContent = process.env.REACT_APP_HOTMAIL_REAL;
-                        }}>{process.env.REACT_APP_HOTMAIL}</h3>
+                            document.getElementById("info-2").textContent = "mercier200012@hotmail.com";
+                        }}>*************</h3>
                         <p className="contact-inspect" onClick={() => {
                             document.getElementById("info-2").style.backgroundColor = "transparent";
-                            document.getElementById("info-2").textContent = process.env.REACT_APP_HOTMAIL_REAL;
+                            document.getElementById("info-2").textContent = "mercier200012@hotmail.com";
                         }}>Click to view Hotmail</p>
                     </div>
                     <div className="contact-item border-right">
                         <h2 className="contact-type">Gmail (Non-professional)</h2>
                         <h3 className="contact-info" id="info-3" onClick={() => {
                             document.getElementById("info-3").style.backgroundColor = "transparent";
-                            document.getElementById("info-3").textContent = process.env.REACT_APP_GMAIL_REAL;
-                        }}>{process.env.REACT_APP_GMAIL}</h3>
+                            document.getElementById("info-3").textContent = "mercier200012@gmail.com";
+                        }}>*************</h3>
                         <p className="contact-inspect" onClick={() => {
                             document.getElementById("info-3").style.backgroundColor = "transparent";
-                            document.getElementById("info-3").textContent = process.env.REACT_APP_GMAIL_REAL;
+                            document.getElementById("info-3").textContent = "mercier200012@gmail.com";
                         }}>Click to view Gmail</p>
                     </div>
                     <div className="contact-item">
                         <h2 className="contact-type">LinkedIn</h2>
                         <h3 className="contact-info" id="info-4" onClick={() => {
                             document.getElementById("info-4").style.backgroundColor = "transparent";
-                            document.getElementById("info-4").textContent = process.env.REACT_APP_LINKEDIN_REAL;
-                        }}>{process.env.REACT_APP_LINKEDIN}</h3>
+                            document.getElementById("info-4").textContent = "titus-mercier-hachey";
+                        }}>*************</h3>
                         <p className="contact-inspect" onClick={() => {
                             document.getElementById("info-4").style.backgroundColor = "transparent";
-                            document.getElementById("info-4").textContent = process.env.REACT_APP_LINKEDIN_REAL;
+                            document.getElementById("info-4").textContent = "titus-mercier-hachey";
                         }}>Click to view LinkedIn</p>
                     </div>
                 </div>
